43fe03db5e4e7e809c09d9d5a77e802a8909583d
ANNA-PC\User <anana96koz@gmail.com>
2018-09-12 03:14:27 +0300

    Первый коммит проба

--- app.py
+++ app.py
@@ -2,3 +2,20 @@ print("hello")
 l = 4
 p = 7
 print(l + p)
+for i in range(1,6,2):
+    print(i)
+print (7 ** 4)
+
+# #Это коммент
+
+
+def fun(str1):
+    print("Строка "+str1)
+    """
+    fuck
+    """
+    return 0
+
+
+fun("пф")
+print(fun.__doc__)
